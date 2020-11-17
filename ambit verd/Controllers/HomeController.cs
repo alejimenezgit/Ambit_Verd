9ab2259d5d316dc68fffd48fb3ebacef85383baa
Alejandro <alejimenezgithub@gmail.com>
2020-11-17 21:09:30 +0100

    Base with translations

---
+++ ambit verd/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ambit_Verd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Change(String Lang)
+        {
+            if(Lang != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(Lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Lang);
+            }
+
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = Lang;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+    }
+}
---
+++ ambit verd/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ambit_Verd.MvcApplication" Language="C#" %>

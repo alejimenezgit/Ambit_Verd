81046ffe75eb7425450c5ebaf33f5e89c90a1b5e
Alejandro <alejimenezgithub@gmail.com>
2021-03-09 22:46:38 +0100

    Push to Main: New stylesheets for the views, new footer 'Create by Alejandro', and the content for the Contact's view

--- ambit verd/App_Start/BundleConfig.cs
+++ ambit verd/App_Start/BundleConfig.cs
@@ -10,6 +10,9 @@ namespace Ambit_Verd
             bundles.Add(new StyleBundle("~/Content/css/layout").Include("~/Content/css/Layout.css"));
             bundles.Add(new StyleBundle("~/Content/css/index").Include("~/Content/css/index.min.css"));
             bundles.Add(new StyleBundle("~/Content/css/services").Include("~/Content/css/Services.min.css"));
+            bundles.Add(new StyleBundle("~/Content/css/contact").Include("~/Content/css/Contact.min.css"));
+            bundles.Add(new StyleBundle("~/Content/css/whoweare").Include("~/Content/css/Whoweare.min.css"));
+            bundles.Add(new StyleBundle("~/Content/css/projects").Include("~/Content/css/Projects.min.css"));
         }
     }
 }
